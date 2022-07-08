d1abf390fb7a51dd866a8c1366e31eaa138b24ef
cpllumaj01 <cpllumaj01@gmail.com>
2022-07-08 09:31:05 -0400

    etch a sketch done

---
+++ script.js
@@ -0,0 +1,122 @@
+const body = document.querySelector('body');
+
+const container1 = document.createElement('div');
+const container2 = document.createElement('div');
+const container3 = document.createElement('div');
+const container4 = document.createElement('div');
+const div1 = document.createElement('div');
+const div2 = document.createElement('div');
+const div3 = document.createElement('div');
+const div4 = document.createElement('div');
+const div5 = document.createElement('div');
+const div6 = document.createElement('div');
+const div7 = document.createElement('div');
+const div8 = document.createElement('div');
+const div9 = document.createElement('div');
+const div10 = document.createElement('div');
+const div11 = document.createElement('div');
+const div12 = document.createElement('div');
+const div13 = document.createElement('div');
+const div14 = document.createElement('div');
+const div15 = document.createElement('div');
+const div16 = document.createElement('div');
+
+container1.classList.add('container');
+container2.classList.add('container');
+container3.classList.add('container');
+container4.classList.add('container');
+body.appendChild(container1);
+body.appendChild(container2);
+body.appendChild(container3);
+body.appendChild(container4);
+
+div1.classList.add('sketchBox');
+div2.classList.add('sketchBox');
+div3.classList.add('sketchBox');
+div4.classList.add('sketchBox');
+div5.classList.add('sketchBox');
+div6.classList.add('sketchBox');
+div7.classList.add('sketchBox');
+div8.classList.add('sketchBox');
+div9.classList.add('sketchBox');
+div10.classList.add('sketchBox');
+div11.classList.add('sketchBox');
+div12.classList.add('sketchBox');
+div13.classList.add('sketchBox');
+div14.classList.add('sketchBox');
+div15.classList.add('sketchBox');
+div16.classList.add('sketchBox');
+container1.appendChild(div1);
+container1.appendChild(div2);
+container1.appendChild(div3);
+container1.appendChild(div4);
+container2.appendChild(div5);
+container2.appendChild(div6);
+container2.appendChild(div7);
+container2.appendChild(div8);
+container3.appendChild(div9);
+container3.appendChild(div10);
+container3.appendChild(div11);
+container3.appendChild(div12);
+container4.appendChild(div13);
+container4.appendChild(div14);
+container4.appendChild(div15);
+container4.appendChild(div16);
+
+div1.addEventListener('mouseover', () => {
+    div1.style.backgroundColor = 'black';
+});
+div2.addEventListener('mouseover', () => {
+    div2.style.backgroundColor = 'black';
+});
+div3.addEventListener('mouseover', () => {
+    div3.style.backgroundColor = 'black';
+});
+div4.addEventListener('mouseover', () => {
+    div4.style.backgroundColor = 'black';
+});
+div5.addEventListener('mouseover', () => {
+    div5.style.backgroundColor = 'black';
+});
+div6.addEventListener('mouseover', () => {
+    div6.style.backgroundColor = 'black';
+});
+div7.addEventListener('mouseover', () => {
+    div7.style.backgroundColor = 'black';
+});
+div8.addEventListener('mouseover', () => {
+    div8.style.backgroundColor = 'black';
+});
+div9.addEventListener('mouseover', () => {
+    div9.style.backgroundColor = 'black';
+});
+div10.addEventListener('mouseover', () => {
+    div10.style.backgroundColor = 'black';
+});
+div11.addEventListener('mouseover', () => {
+    div11.style.backgroundColor = 'black';
+});
+div12.addEventListener('mouseover', () => {
+    div12.style.backgroundColor = 'black';
+});
+div13.addEventListener('mouseover', () => {
+    div13.style.backgroundColor = 'black';
+});
+div14.addEventListener('mouseover', () => {
+    div14.style.backgroundColor = 'black';
+});
+div15.addEventListener('mouseover', () => {
+    div15.style.backgroundColor = 'black';
+});
+div16.addEventListener('mouseover', () => {
+    div16.style.backgroundColor = 'black';
+});
+
+
+
+
+
+
+
+
+
